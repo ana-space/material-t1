327900272e65551a57ed0a4d263408e55dcb3ba3
ana <ana@plista.com>
2020-02-25 17:47:36 +0100

    #dAANA 25.02.20

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+  import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'material-t1';
+  notification = 0;
+  opened= true;
+  opened_1= true;
+  log(status) {
+    console.log(status)
+  }
+  toggle(vall) {
+    console.log(vall);
+    this.opened_1 = !vall;
+    setTimeout((vall) => {
+      this.opened_1 = !this.opened_1;
+    }, 500);
+  }
+}
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu'
+// import { from } from 'rxjs';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatSidenavModule,
+  MatMenuModule
+]
+
+@NgModule({
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
